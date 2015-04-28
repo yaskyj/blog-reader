7b7a283d65e644f75d71d0dd542df6d603daab77
yaskyj <yaskyj@yahoo.com>
2015-04-27 19:10:59 -0500

    Add truncated toolbar, functionality for showing-hiding toolbar on editor focus, and Bootstrap layout to editor-toolbar

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,7 +1,7 @@
 'use strict';
 blogReader.controller('MainCtrl', function ($scope) {
   $scope.entryText = '';
-
+  $scope.editorFocus = false;
   //Testing blog updating
   // $scope.$watch('[entryText]', function() {
   //       console.log($scope.entryText);
